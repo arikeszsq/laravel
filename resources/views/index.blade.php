d9031ff7804440f645cf55635d5f7c5481f1ddfa
zhusq <18036161805@163.com>
2020-09-15 12:52:05 +0800

    init laravel

---
+++ resources/views/index.blade.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script type="text/javascript" src="{{ URL::asset('/') }}/lib/jquery/jquery-2.1.1.min.js"></script>
+    <script type="text/javascript" src="{{ URL::asset('/') }}/lib/bootstrap-3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="{{ URL::asset('/') }}/lib/bootstrap-3.3.7/css/bootstrap.min.css">
+    <script src=""></script>
+    <title>Document</title>
+</head>
+<style>
+    .new_row {
+        padding: 1rem;
+        border-bottom: 1px grey dashed;
+    }
+
+    .submit_row {
+        margin-top: 1rem;
+        text-align: center;
+    }
+
+    input[type="radio"] {
+        position: absolute;
+        left: -999em;
+    }
+
+    label {
+        font-weight: bolder;
+        font-size: 16px;
+    }
+</style>
+
+<body>
+
+<div class="container">
+    <form action="./" method="get" id="formid">
+        <div class="imp_content">
+            <div class="row new_row">
+                <div class="col-md-1 col-xs-1"></div>
+                <div class="col-md-2 col-xs-11 title_label"><label for="num">type</label></div>
+                <div class="col-md-4 col-xs-4"><label><input name="type" type="radio" value="1" checked/>
+                        <div class="btn btn-danger">consume</div>
+                    </label></div>
+                <div class="col-md-4 col-xs-4"><label><input name="type" type="radio" value="2"/>
+                        <div class="btn btn-warning">income</div>
+                    </label></div>
+                <div class="col-md-1 col-xs-2"></div>
+            </div>
+            <div class="row new_row">
+                <div class="col-md-1 col-xs-1"></div>
+                <div class="col-md-2 col-xs-11 title_label"><label for="num">num</label></div>
+                <div class="col-md-8 col-xs-8"><input type="text" name="num" id="num"></div>
+                <div class="col-md-1 col-xs-1"></div>
+            </div>
+            <div class="row new_row">
+                <div class="col-md-1 col-xs-1"></div>
+                <div class="col-md-2 col-xs-11 title_label"><label for="content">content</label></div>
+                <div class="col-md-8 col-xs-8"><textarea row="10" cols="20" name="content">备注</textarea></div>
+                <div class="col-md-1 col-xs-1"></div>
+            </div>
+            <div class="row submit_row">
+                <div class="col-md-1 col-xs-1"></div>
+                <div class="col-md-2 col-xs-11"><input type="button" class="btn btn-success submit" value="submit">
+                </div>
+                <div class="col-md-8 col-xs-8"></div>
+                <div class="col-md-1 col-xs-1"></div>
+            </div>
+
+        </div>
+
+    </form>
+</div>
+<script>
+    $('.submit').click(function () {
+
+        if (1) {
+            $('#formid').submit();
+        }
+    });
+</script>
+<script type="text/javascript">
+    $(document).ready(function () {
+        $('label').click(function () {
+            if ($(this).children('input').is(":checked")) {
+                $(this).css('background-color', '#f7edb8');
+                $(this).nextAll().css('background-color', '#f8fbf9');
+                $(this).prevAll().css('background-color', '#f8fbf9');
+            }
+        })
+    })
+</script>
+
+</body>
+
+</html>
---
+++ resources/views/lib_jq/bootstrap3-dialog/src/js/bootstrap-dialog-tool.js
@@ -0,0 +1,138 @@
+﻿jQuery(document).ready(function () {
+
+
+
+    //弹出警示提示的登录框
+    $.showWaring = function (str, func) {
+        // 调用show方法
+        BootstrapDialog.show({
+            type: BootstrapDialog.TYPE_WARNING,
+            title: '提醒 ',
+            message: str,
+            size: BootstrapDialog.SIZE_SMALL, //size为小，默认的对话框比较宽
+            buttons: [{// 设置关闭按钮
+                label: '关闭',
+                cssClass: 'btn-primary',
+                action: function (dialogItself) {
+                    dialogItself.close();
+                }
+            }],
+            onshown: function (dialogRef) {
+                setTimeout(function () {
+                    dialogRef.close();
+                }, 5000);
+            },
+            // 对话框关闭时带入callback方法
+            onhide: func
+        });
+    };
+
+
+
+    //弹出错误提示的登录框
+    $.showError = function (str, func) {
+        // 调用show方法
+        BootstrapDialog.show({
+            type: BootstrapDialog.TYPE_DANGER,
+            title: '错误 ',
+            message: str,
+            size: BootstrapDialog.SIZE_SMALL, //size为小，默认的对话框比较宽
+            buttons: [{// 设置关闭按钮
+                label: '关闭',
+                cssClass: 'btn-primary',
+                action: function (dialogItself) {
+                    dialogItself.close();
+                }
+            }],
+            // 对话框关闭时带入callback方法
+            onhide: func
+        });
+    };
+
+
+    $.showConfirm = function (str) {
+          BootstrapDialog.confirm({
+            title: '确认',
+            message:str,
+            type: BootstrapDialog.TYPE_WARNING, // < - 默认值为BootstrapDialog.TYPE_PRIMARY
+            closable: true, // < - 默认值为false
+            draggable: true, // < - 默认值为false
+            btnOKLabel: '确认', // < - 默认值为'OK'，
+            btnCancelLabel:'取消',
+            btnOKClass: 'btn-warning', // < - 如果你没有指定它，将使用对话框类型，
+            size: BootstrapDialog.SIZE_SMALL,
+            onhide:function () {
+        
+            },
+            callback: function (result) {
+                //如果单击按钮，则结果为true;如果用户直接关闭对话框，则结果为false。
+                if (result) {
+                   //Func(mydata);
+
+                   alert(22222);
+                } 
+            }
+        });
+    };
+
+
+
+    //弹出错误提示的登录框
+    $.showSuccess = function (str, func) {
+        // 调用show方法
+        BootstrapDialog.show({
+            type: BootstrapDialog.TYPE_SUCCESS,
+            title: '成功 ',
+            message: str,
+            size: BootstrapDialog.SIZE_SMALL, //size为小，默认的对话框比较宽
+            buttons: [{// 设置关闭按钮
+                label: '关闭',
+                cssClass: 'btn-primary',
+                action: function (dialogItself) {
+                    dialogItself.close();
+                }
+            }],
+            // 对话框关闭时带入callback方法
+            onhide: func
+        });
+    };
+
+
+    $.showSuccessTimeout = function (str, func) {
+        BootstrapDialog.show({
+            type: BootstrapDialog.TYPE_SUCCESS,
+            title: '成功 ',
+            message: str,
+            size: BootstrapDialog.SIZE_SMALL,
+            buttons: [{
+                label: '确定',
+                cssClass: 'btn-primary',
+                action: function (dialogItself) {
+                    dialogItself.close();
+                }
+            }],
+            // 指定时间内可自动关闭
+            onshown: function (dialogRef) {
+                setTimeout(function () {
+                    dialogRef.close();
+                }, 5000);
+            },
+            onhide: func
+        });
+    };
+
+
+    $.showremote = function (title,url, func) {
+        BootstrapDialog.show({
+            title: title,
+            message: $('<div></div>').load(url),
+            buttons: [{
+                label: '确定',
+                cssClass: 'btn-primary',
+                action: function (dialogItself) {
+                    dialogItself.close();
+                }
+            }],
+        });
+     };
+});
