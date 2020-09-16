a923f7d5ec8fbc06b8cb851de5d1cdca8e3f7e35
zhusq <18036161805@163.com>
2020-09-16 11:27:46 +0800

    增加资料等信息

--- app/Admin/Controllers/ZttWebController.php
+++ app/Admin/Controllers/ZttWebController.php
@@ -28,12 +28,17 @@ class ZttWebController extends AdminController
 
         $grid->column('id', __('ID'))->color('red')->hide();
         $grid->column('title', __('名称'));
-        $grid->column('is_h5', __('是H5'));
-        $grid->column('money', __('金额'));
+//        $grid->column('is_h5', __('是H5'));
+//        $grid->column('money', __('金额'));
         $grid->column('count_money', __('计算金额'));
         $grid->column('contacter', __('联系人'));
         $grid->column('mobile', __('手机号'));
         $grid->column('ensure_picture', __('确认效果图'));
+
+        $grid->column('program', __('程序'));
+        $grid->column('info', __('资料'));
+        $grid->column('online', __('上线'));
+
         $grid->column('domain', __('域名'));
         $grid->column('status', __('状态'));
         $grid->column('updated_at', __('更新时间'))->hide();
@@ -54,12 +59,15 @@ class ZttWebController extends AdminController
 
         $show->field('id', __('Id'));
         $show->field('title', __('名称'));
-        $show->field('is_h5', __('是H5'));
-        $show->field('money', __('金额'));
+//        $show->field('is_h5', __('是H5'));
+//        $show->field('money', __('金额'));
         $show->field('count_money', __('计算金额'));
         $show->field('contacter', __('联系人'));
         $show->field('mobile', __('手机号'));
         $show->field('ensure_picture', __('确认效果图'));
+        $show->field('program', __('程序'));
+        $show->field('info', __('资料'));
+        $show->field('online', __('上线'));
         $show->field('domain', __('域名'));
         $show->field('status', __('状态'));
         $show->field('updated_at', __('更新时间'));
@@ -84,12 +92,15 @@ class ZttWebController extends AdminController
         $form = new Form(new ZttWeb());
 
         $form->text('title', __('名称'));
-        $form->switch('is_h5', __('是H5'));
-        $form->text('money', __('金额'));
+//        $form->switch('is_h5', __('是H5'));
+//        $form->text('money', __('金额'));
         $form->text('count_money', __('计算金额'));
         $form->text('contacter', __('联系人'));
         $form->text('mobile', __('手机号'));
         $form->switch('ensure_picture', __('确认效果图'));
+        $form->switch('program', __('程序'));
+        $form->switch('info', __('资料'));
+        $form->switch('online', __('上线'));
         $form->text('domain', __('域名'));
         $form->select('status', '状态')->options($directors);
         $form->footer(function ($footer) {
