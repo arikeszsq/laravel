4c700e1935310fb26436cc067335cece1ec5a3e3
zhusq <18036161805@163.com>
2020-10-09 14:47:37 +0800

    增加学习接口

---
+++ app/Http/Controllers/Vue/LearnController.php
@@ -0,0 +1,31 @@
+<?php
+
+
+namespace App\Http\Controllers\Vue;
+
+
+use Illuminate\Support\Facades\DB;
+
+class LearnController extends BaseController
+{
+    public function list()
+    {
+        $params = request()->all();
+        $type = isset($params['type']) && $params['type'] ? $params['type'] : '';
+        $data = [];
+//        if ($type) {
+        $list = DB::table('zsq_everyday_poetry')->get();
+        foreach ($list as $item) {
+            $data[] = [
+                'title' => $item->title,
+                'author' => $item->author,
+                'description' => $item->description,
+                'content' => $item->content,
+                'create_time' => $item->create_time
+            ];
+        }
+//        }
+        return $this->returnRet($data);
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -3,28 +3,15 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-
 
 Route::any('money/add', 'MoneyController@add');
 Route::any('money/getCountMoney', 'MoneyController@getCountMoney');
 Route::any('money/getList', 'MoneyController@getList');
 Route::any('basic/info', 'BasicInfoController@info');
 Route::any('mail/send', 'MailController@send');
-
 
 Route::group(['namespace' => 'Vue', 'prefix' => 'vue'], function () {
     Route::any('basic/info', 'BasicInfoController@info');
@@ -35,6 +22,10 @@ Route::group(['namespace' => 'Vue', 'prefix' => 'vue'], function () {
     Route::group(['prefix' => 'salary'], function () {
         Route::any('add', 'SalaryController@add');
         Route::any('delete', 'SalaryController@delete');
+    });
+    /**Learn接口**/
+    Route::group(['prefix' => 'learn'], function () {
+        Route::any('list', 'LearnController@list');
     });
 });
 
@@ -48,6 +39,3 @@ Route::group(['namespace' => 'Vue', 'middleware' => 'cors', 'prefix' => 'vue'], 
  * 同时，添加一个 prefix 是为了版本号，方便后期接口升级区分。
  * 打开 postman, 用 get 方式请求你的域名/api/v1/users
  */
-//Route::namespace('Api')->prefix('v1')->group(function () {
-//    Route::get('/users', 'UserController@index')->name('users.index');
-//});
