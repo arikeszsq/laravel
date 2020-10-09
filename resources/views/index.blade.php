87de7b14bd5f7cec59cd6b3a9eab64ff769ed143
zhusq <18036161805@163.com>
2020-10-09 15:30:33 +0800

    修改首页

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -50,68 +50,7 @@
 </style>
 
 <body>
-
 <div class="basic_info" style="text-align: center;margin:10px auto;"></div>
-
-<div class="container">
-    {{--        <form action="/api/money/add" method="POST" id="formid">--}}
-    <form method="POST" id="formid">
-        <div class="imp_content">
-            <div class="row new_row">
-                <div class="col-md-1 col-xs-1"></div>
-                <div class="col-md-2 col-xs-11 title_label"><label for="num">type</label></div>
-                <div class="col-md-4 col-xs-4"><label><input name="type" type="radio" value="1" checked/>
-                        <div class="btn btn-danger">out</div>
-                    </label></div>
-                <div class="col-md-4 col-xs-4"><label><input name="type" type="radio" value="2"/>
-                        <div class="btn btn-warning">in</div>
-                    </label></div>
-                <div class="col-md-1 col-xs-2"></div>
-            </div>
-            <div class="row new_row">
-                <div class="col-md-1 col-xs-1"></div>
-                <div class="col-md-2 col-xs-11 title_label"><label for="num">num</label></div>
-                <div class="col-md-8 col-xs-8"><input type="text" name="num" id="num" class="num"></div>
-                <div class="col-md-1 col-xs-1"></div>
-            </div>
-            <div class="row new_row">
-                <div class="col-md-1 col-xs-1"></div>
-                <div class="col-md-2 col-xs-11 title_label"><label for="content">content</label></div>
-                <div class="col-md-8 col-xs-8"><textarea row="10" cols="20" name="content" class="content">备注</textarea>
-                </div>
-                <div class="col-md-1 col-xs-1"></div>
-            </div>
-            <div class="row submit_row">
-                <div class="col-md-1 col-xs-1"></div>
-                <div class="col-md-2 col-xs-11"><input type="button" class="btn btn-success submit" value="submit">
-                </div>
-                <div class="col-md-8 col-xs-8"></div>
-                <div class="col-md-1 col-xs-1"></div>
-            </div>
-
-        </div>
-
-    </form>
-    <span class="notice_type"></span>
-    {{--    <span class="notice_num"></span>--}}
-    {{--    <span class="notice_content"></span>--}}
-
-    <div class="count_info"></div>
-    <table border=1  width="180" height="60">
-        <tr>
-            <td>张天龙</td>
-            <td>陈阳</td>
-            <td>许兴元</td>
-            <td>邱伏奇</td>
-        </tr>
-        <tr>
-            <td>陈子安</td>
-            <td>宋贝贝</td>
-            <td>历凯</td>
-        </tr>
-    </table>
-
-</div>
 <script>
     $(document).ready(function () {
         $.ajax({
