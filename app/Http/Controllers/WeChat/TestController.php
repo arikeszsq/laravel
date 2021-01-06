9a0a39603edc645766a4d515ed326f9cc37add3c
zhusq <18036161805@163.com>
2021-01-06 17:04:39 +0800

    commit

---
+++ app/Http/Controllers/WeChat/TestController.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace App\Http\Controllers\WeChat;
+
+
+use App\Models\ZsqMoneyConsume;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class TestController extends BaseController
+{
+}
